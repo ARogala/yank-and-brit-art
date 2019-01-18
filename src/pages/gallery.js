5d8b79fd42c778a4681c39f08560cfd79034f611
Andrew Rogala <adrogala87@gmail.com>
2019-01-18 10:48:18 -0500

    move images to new folder

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import Gallery from 'react-photo-gallery';
 import Layout from '../components/layout';
 
-import p1 from '../img/uploads/gallery1.jpg';
-import p2 from '../img/uploads/gallery2.jpg';
-import p3 from '../img/uploads/gallery3.jpg';
-import p4 from '../img/uploads/gallery4.jpg';
+import p1 from '../img/gallery/gallery1.jpg';
+import p2 from '../img/gallery/gallery2.jpg';
+import p3 from '../img/gallery/gallery3.jpg';
+import p4 from '../img/gallery/gallery4.jpg';
 
 const Photos = [
 	{
@@ -32,6 +33,7 @@ const Photos = [
 
 class GalleryComponent extends React.Component {
 	render() {
+		console.log(this.props.data);
 		return(
 			<Layout>
 				<Gallery photos={Photos} />
@@ -41,3 +43,15 @@ class GalleryComponent extends React.Component {
 }
 
 export default GalleryComponent;
+
+export const query = graphql`
+	query {
+		allFile(filter: { extension: { eq: "jpg" }, relativeDirectory: {eq: "img/gallery"} }) {
+			edges {
+				node {
+					relativePath
+				}
+			}
+		}
+	}
+`
